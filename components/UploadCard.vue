a21418a8f1de3ffe3b8e401618446d4eb71415bb
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-02-17 12:55:56 -0800

    first commit

---
+++ components/UploadCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <section
+    class="flex h-[469px] w-[402px] flex-col items-center justify-around rounded-xl bg-white py-9 shadow-lg"
+  >
+    <h1 class="text-lg">Upload your image</h1>
+    <span class="text-[10px] text-[#828282]">File should be Jpeg, Png,...</span>
+    <input
+      class="h-[219px] w-[338px] rounded-xl border-[2px] border-dashed border-[#97BEF4] bg-[#F6F8FB]"
+      type="file"
+      @change="onFileChange"
+    />
+    <span class="text=[12px] text-[#BDBDBD]">or</span>
+    <button
+      class="rounded-lg bg-[#2F80ED] px-4 py-2 text-xs text-white"
+      @click="onUpload"
+    >
+      Choose a file
+    </button>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="grid h-screen place-items-center bg-[#FAFAFB]">
+    <UploadCard />
+  </div>
+</template>
+
+<script>
+import UploadCard from '@/components/UploadCard.vue'
+export default { components: { UploadCard } }
+</script>
