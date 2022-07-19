224eb0f99e395f8e9383c563b6edf28c94e4b1c4
Rafael Amed Valenzuela <69322700+rafamedtech@users.noreply.github.com>
2022-07-18 18:26:48 -0700

    auth test

--- store/index.js
+++ store/index.js
@@ -9,12 +9,13 @@ export const state = () => ({
 export const actions = {
   async nuxtServerInit({ dispatch }) {
     await dispatch('pins/fetchPins')
-    if (this.$auth.$storage.getUniversal('user')) {
-      this.$auth.setUser(this.$auth.$storage.getUniversal('user'))
-      await this.$auth.setUserToken(
-        this.$auth.$storage.getUniversal('token'),
-        this.$auth.$storage.getUniversal('refresh')
-      )
+    if (this.$auth.user) {
+      // this.$auth.setUser(this.$auth.$storage.getUniversal('user'))
+      // await this.$auth.setUserToken(
+      //   this.$auth.$storage.getUniversal('token'),
+      //   this.$auth.$storage.getUniversal('refresh')
+      // )
+      console.log('user', this.$auth.user)
     }
   },
 
@@ -45,10 +46,10 @@ export const actions = {
         data: payload,
       })
 
-      this.$auth.setUser(data.user)
+      // this.$auth.setUser(data.user)
 
-      this.$auth.$storage.setUniversal('token', data.access)
-      this.$auth.$storage.setUniversal('refresh', data.refresh)
+      // this.$auth.$storage.setUniversal('token', data.access)
+      // this.$auth.$storage.setUniversal('refresh', data.refresh)
       this.$auth.$storage.setUniversal('user', data.user)
       await this.$auth.setUserToken(data.access, data.refresh)
       // console.log(this.$auth.user)
